23a318ff5c888abc39c83afd73b2eeab841c65d1
Alem Mohamed <126617660+moumouh6@users.noreply.github.com>
2025-03-04 17:46:15 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Gestion du menu hamburger
+    const hamburger = document.querySelector(".hamburger");
+    const navLinks = document.querySelector(".nav-links");
+
+    hamburger.addEventListener("click", function () {
+        navLinks.classList.toggle("active");
+    });
+
+    // Gestion des boutons de connexion et d'inscription
+    document.querySelector(".button2").addEventListener("click", function () {
+        window.location.href = "https://am11iin.github.io/sign-in/";
+    });
+
+    document.querySelector(".button3").addEventListener("click", function () {
+        window.location.href = "https://moumouh6.github.io/Log-in/";
+       
+    });
+    document.querySelector(".button1").addEventListener("click", function () {
+        window.location.href = "https://moumouh6.github.io/Log-in/";
+    });
+});
